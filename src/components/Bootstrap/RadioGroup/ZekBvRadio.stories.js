93fa9ba3641ed65cde39d648ce2a6cc921a8b7ec
Yusuf Özdemir <yusuf.o@cyberneticlabs.io>
2024-02-03 14:57:55 +0300

    ZEK-2219 added radio component and story, update storybook

---
+++ src/components/Bootstrap/RadioGroup/ZekBvRadio.stories.js
@@ -0,0 +1,67 @@
+import ZekBvRadio from './ZekBvRadio.vue'
+
+export default {
+  title: 'Bootstrap/ZekBvRadio',
+  component: ZekBvRadio,
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      control: { type: 'text' }
+    },
+    customClass: { type: 'text' },
+    customStyle: { type: 'object' },
+    required: {
+      control: { type: 'boolean' }
+    },
+    disabled: {
+      control: { type: 'boolean' }
+    },
+    name: {
+      control: { type: 'text' }
+    },
+    id: {
+      control: { type: 'text' }
+    },
+    size: {
+      control: { type: 'text' }
+    },
+    description: {
+      control: { type: 'text' }
+    },
+    type: {
+      control: { type: 'select' },
+      options: [
+        'buttons',
+        'plain'
+      ]
+    },
+    onInput: { control: 'action', action: 'input' }, 
+    onChange: { control: 'action', action: 'change' },
+  }
+}
+
+const Template = (args, { argTypes }) => ({
+  setup() {
+    return { args }
+  },
+  props: Object.keys(argTypes),
+  components: { ZekBvRadio },
+  template: '<ZekBvRadio v-bind="args" />'
+})
+
+export const Default = Template.bind({})
+Default.args = {
+    value: 'second',
+    items: [
+        {text: 'First Check', value: 'first', disabled: true},
+        {text: 'Second Check', value: 'second'},
+        {text: 'Third Check', value: 'third'},
+    ],
+    // items: ['a', 'b', 'c'],
+    // size: 'md',
+    // successMessage: 'success',
+    // errorMessage: 'error',
+    // label: 'checkbox',
+    // required: true,
+    // description: 'please select two',
+}
