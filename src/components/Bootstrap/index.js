07869e245139104e6e0c7d4c1c74121da936fa1d
AbdAsh <a.mahmutoglu@cyberneticlabs.io>
2024-02-06 21:22:37 +0300

    Bootstrap Monorepo config added

---
+++ src/components/Bootstrap/index.js
@@ -0,0 +1,9 @@
+import ZekBvCheckbox from './Checkbox/ZekBvCheckbox.vue'
+import ZekBvDropdown from './Dropdown/ZekBvDropdown.vue'
+import ZekBvInput from './InputField/ZekBvInput.vue'
+import ZekBvRadio from './RadioGroup/ZekBvRadio.vue'
+import ZekBvTextArea from './Texarea/ZekBvTextarea.vue'
+
+export { ZekBvCheckbox, ZekBvDropdown, ZekBvInput, ZekBvRadio, ZekBvTextArea }
+
+export default new Error('Only named exports are supported')
