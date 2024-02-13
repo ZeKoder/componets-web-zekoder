423ff2052ad3b80f4d3bcfb29b5006e38adf056d
Yusuf Özdemir <yusuf.o@cyberneticlabs.io>
2024-02-13 19:08:12 +0300

    add file upload import to index.js

--- src/components/Bootstrap/index.js
+++ src/components/Bootstrap/index.js
@@ -3,7 +3,8 @@ import ZekBvDropdown from './Dropdown/ZekBvDropdown.vue'
 import ZekBvInput from './InputField/ZekBvInput.vue'
 import ZekBvRadio from './RadioGroup/ZekBvRadio.vue'
 import ZekBvTextArea from './Texarea/ZekBvTextarea.vue'
+import ZekBvFileUpload from './FileUpload/ZekBvFileUpload.vue'
 
-export { ZekBvCheckbox, ZekBvDropdown, ZekBvInput, ZekBvRadio, ZekBvTextArea }
+export { ZekBvCheckbox, ZekBvDropdown, ZekBvInput, ZekBvRadio, ZekBvTextArea, ZekBvFileUpload }
 
 export default new Error('Only named exports are supported')
